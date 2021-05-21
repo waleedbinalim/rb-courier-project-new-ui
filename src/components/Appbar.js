f6a8da907b90b46ef842bc0941dce7e5f25130a2
waleedbinalim <waleedbinalim@gmail.com>
2021-05-21 22:57:07 +0500

    added SPA functionality and Logout Button

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React ,{useContext} from 'react'
+import { useHistory , NavLink } from "react-router-dom";
 import {AppBar , Toolbar, Avatar} from '@material-ui/core'
 import {List , ListItem , ListItemText , IconButton} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
 import {makeStyles} from '@material-ui/core'
-import {navLinks} from '../constants/Constants'
+import {navLinks , logout} from '../constants/Constants'
+import {AuthContext} from '../state/auth/AuthContext'
 
 let useStyles = makeStyles((theme) => {
     return{
@@ -46,6 +48,18 @@ let useStyles = makeStyles((theme) => {
 })
 
 export default function Appbar({openDrawer}) {
+    const history = useHistory();
+
+    const {isLoggedIn , setIsLoggedIn} = useContext(AuthContext);
+
+
+    const handleLogout = async () => {
+        const logOut = await fetch(logout , {credentials: 'include'} )
+        const loggedOut = await logOut.json();
+        console.log(loggedOut)
+        setIsLoggedIn(false)
+        history.push('/')
+    }
 
     const classes = useStyles();
 
@@ -61,25 +75,32 @@ export default function Appbar({openDrawer}) {
                         <List  className={classes.navDisplayFlex}>
                             {navLinks.map(link => {
                                 return(
-                                    <a href={link.path} key={link.title} className={classes.linkText}>
+                                    <NavLink to={link.path} key={link.title} className={classes.linkText}>
                                         <ListItem button>
                                             <ListItemText primary={link.title}  />
                                         </ListItem>
-                                    </a>
+                                    </NavLink>
                                 )
                             })}
                         </List >
                             <List className={classes.auth}>
-                                        <a href="/login" className={classes.linkText}>
+                                        <NavLink to="/login" className={classes.linkText}>
                                             <ListItem button>
                                                 <ListItemText primary="Login"  />
                                             </ListItem>
-                                        </a>
-                                        <a href="/signup" className={classes.linkText}>
+                                        </NavLink>
+                                        <NavLink to="/signup" className={classes.linkText}>
                                             <ListItem button>
                                                 <ListItemText primary="Sign Up"  />
                                             </ListItem>
-                                        </a>
+                                        </NavLink>
+
+                                        <div className={classes.linkText} onClick={() => {handleLogout()}}>
+                                        <ListItem button>
+                                            <ListItemText primary="Log Out"  />
+                                        </ListItem>
+                                        </div>
+
                             </List>
                     </div>
                 </div>
