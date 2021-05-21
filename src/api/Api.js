63d3c09d1ac6d712f6c8d4c1aff306aa1babf896
waleedbinalim <waleedbinalim@gmail.com>
2021-05-21 22:57:29 +0500

    Eliminated CORS errors

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,4 +1,9 @@
 import axios from 'axios'
 
-export const headers = {'Content-Type': 'application/json;charset=UTF-8',"Access-Control-Allow-Origin": "*"}
+axios.defaults.withCredentials = true
+
+export const headers = {'Content-Type': 'application/json;charset=UTF-8',
+"Access-Control-Allow-Origin": "http://localhost:5000",
+'Access-Control-Allow-Credentials': 'true'
+}
 export const api = axios.create({baseURL: 'http://localhost:5000'})
