027872675a4567c8f92e6d85e52804c05a32fdeb
Waleed Bin Alim <waleedbinalim@gmail.com>
2021-06-09 16:41:13 +0500

    recieved state as props, fixed formatting

--- src/components/orders/OrderEditDialogue.js
+++ src/components/orders/OrderEditDialogue.js
@@ -1,55 +1,62 @@
-import React , {useContext} from "react";
+import React, { useContext } from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import {makeStyles} from "@material-ui/core";
-import {headers ,api} from '../../api/Api'
-import {OrderContext} from '../../state/orders/OrderContext'
+import { makeStyles } from "@material-ui/core";
+import { headers, api } from '../../utils/api/Api'
+import { OrderContext } from '../../state/orders/OrderContext'
 
 const useStyles = makeStyles((theme) => {
-    return{
-        editForm: {
-            padding: 24
-        },
-        fields:{
-            margin: '12px 0px',
-            display: 'block',
-            padding: 16
-        },
-        dialoguePaper	:	{
-          borderRadius: 16,
-          minWidth: '70vw'
-        }
+  return {
+    editForm: {
+      padding: 24
+    },
+    fields: {
+      margin: '12px 0px',
+      display: 'block',
+      padding: 16
+    },
+    dialoguePaper: {
+      borderRadius: 16,
+      minWidth: '70vw'
     }
+  }
 })
 
-export default function SimpleDialogDemo( ) {
-    const {setFilteredOrders , setOrders , openEditModal, setOpenEditModal , editOrderValue , setEditOrderValue} = useContext(OrderContext)
-    const classes = useStyles();
-    const handleClose = () => {
-        setOpenEditModal(false);
-    };
+export default function SimpleDialogDemo({ setOrders, setFilteredOrders }) {
+  const {
+    // setOrders,
+    // setFilteredOrders,
+    openEditModal,
+    setOpenEditModal,
+    editOrderValue,
+    setEditOrderValue
+  } = useContext(OrderContext)
+  const classes = useStyles();
+  const handleClose = () => {
+    setOpenEditModal(false);
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        let url = '/orders/' + editOrderValue._id
-        let data = JSON.stringify(editOrderValue)
-        await api.put( url , data , {headers: headers})
-        let updatedOrders = await api.get('/orders/all')
-        setOrders(updatedOrders.data)
-        setFilteredOrders(updatedOrders.data)
-        setOpenEditModal(false)
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    let url = '/orders/' + editOrderValue._id
+    let data = JSON.stringify(editOrderValue)
+    await api.put(url, data, { headers: headers })
+    let updatedOrders = await api.get('/orders/all')
+    setOrders(updatedOrders.data)
+    setFilteredOrders(updatedOrders.data)
+    setOpenEditModal(false)
+  }
 
 
   return (
     <div>
-      <Dialog classes={{paper: classes.dialoguePaper}} borderRadis={16} onClose={handleClose} open={openEditModal}>
-      <form className={classes.editForm} onSubmit={handleSubmit}>
-          <input className={classes.fields} type="text" defaultValue={editOrderValue.deliveredTo} onKeyUp={(e) => setEditOrderValue({...editOrderValue , deliveredTo : e.target.value})}/>
-          <input className={classes.fields} type="text" defaultValue={editOrderValue.deliveredBy} onKeyUp={(e) => setEditOrderValue({...editOrderValue , deliveredBy : e.target.value})}/>
-        <Button type="submit" variant="contained" color="primary" >SUBMIT</Button>
-      </form>
-    </Dialog>
+      <Dialog classes={{ paper: classes.dialoguePaper }} borderRadis={16} onClose={handleClose} open={openEditModal}>
+        <form className={classes.editForm} onSubmit={handleSubmit}>
+          <input className={classes.fields} type="text" defaultValue={editOrderValue.deliveredTo} onKeyUp={(e) => setEditOrderValue({ ...editOrderValue, deliveredTo: e.target.value })} />
+          <input className={classes.fields} type="text" defaultValue={editOrderValue.deliveredBy} onKeyUp={(e) => setEditOrderValue({ ...editOrderValue, deliveredBy: e.target.value })} />
+          <Button type="submit" variant="contained" color="primary" >SUBMIT</Button>
+        </form>
+      </Dialog>
     </div>
   );
 }
--- src/components/users/UserEditDialogue.js
+++ src/components/users/UserEditDialogue.js
@@ -1,56 +1,63 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import {makeStyles} from "@material-ui/core";
-import {headers ,api} from '../../api/Api'
-import {UserContext} from '../../state/users/UserContext'
+import { makeStyles } from "@material-ui/core";
+import { headers, api } from '../../utils/api/Api'
+import { UserContext } from '../../state/users/UserContext'
 
 const useStyles = makeStyles((theme) => {
-    return{
-        editForm: {
-            padding: 24
-        },
-        fields:{
-            margin: '12px 0px',
-            display: 'block',
-            padding: 16
-        },
-    }
+  return {
+    editForm: {
+      padding: 24
+    },
+    fields: {
+      margin: '12px 0px',
+      display: 'block',
+      padding: 16
+    },
+  }
 })
 
-export default function SimpleDialogDemo() {
-    const {setFilteredUsers , setUsers, openEditModal , setOpenEditModal , editUserValue , setEditUserValue} = useContext(UserContext)
-    const classes = useStyles();
+export default function UserEditDialogue({ setUsers, setFilteredUsers }) {
+  const {
+    // setFilteredUsers,
+    // setUsers,
+    openEditModal,
+    setOpenEditModal,
+    editUserValue,
+    setEditUserValue
+  } = useContext(UserContext)
+  const classes = useStyles();
 
-    
-    const handleClose = () => {
-        setOpenEditModal(false);
-    };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        let url = '/users/' + editUserValue._id
-        let data = JSON.stringify(editUserValue)
-        await api.put( url , data , {headers: headers})
-        let updatedUsers = await api.get('/users/all')
-        setUsers(updatedUsers.data)
-        setFilteredUsers(updatedUsers.data)
-        setOpenEditModal(false)
-    }
+  const handleClose = () => {
+    setOpenEditModal(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    let url = '/users/' + editUserValue._id
+    let data = JSON.stringify(editUserValue)
+    await api.put(url, data, { headers: headers })
+    let updatedUsers = await api.get('/users/all')
+    setUsers(updatedUsers.data)
+    setFilteredUsers(updatedUsers.data)
+    setOpenEditModal(false)
+  }
 
 
   return (
     <div>
       <Dialog onClose={handleClose} open={openEditModal}>
-      <form className={classes.editForm} onSubmit={handleSubmit}>
-          <input className={classes.fields} type="text" defaultValue={editUserValue.name} onKeyUp={(e) => setEditUserValue({...editUserValue , name : e.target.value})}/>
-          <input className={classes.fields} type="text" defaultValue={editUserValue.email} onKeyUp={(e) => setEditUserValue({...editUserValue , email : e.target.value})}/>
-          <input className={classes.fields} type="text" defaultValue={editUserValue.city} onKeyUp={(e) => setEditUserValue({...editUserValue , city : e.target.value})}/>
-          <input className={classes.fields} type="text" defaultValue={editUserValue.address} onKeyUp={(e) => setEditUserValue({...editUserValue , address : e.target.value})}/>
-          <input className={classes.fields} type="text" defaultValue={editUserValue.phone} onKeyUp={(e) => setEditUserValue({...editUserValue , phone : e.target.value})}/>
-        <Button type="submit" variant="contained" color="primary" >SUBMIT</Button>
-      </form>
-    </Dialog>
+        <form className={classes.editForm} onSubmit={handleSubmit}>
+          <input className={classes.fields} type="text" defaultValue={editUserValue.name} onKeyUp={(e) => setEditUserValue({ ...editUserValue, name: e.target.value })} />
+          <input className={classes.fields} type="text" defaultValue={editUserValue.email} onKeyUp={(e) => setEditUserValue({ ...editUserValue, email: e.target.value })} />
+          <input className={classes.fields} type="text" defaultValue={editUserValue.city} onKeyUp={(e) => setEditUserValue({ ...editUserValue, city: e.target.value })} />
+          <input className={classes.fields} type="text" defaultValue={editUserValue.address} onKeyUp={(e) => setEditUserValue({ ...editUserValue, address: e.target.value })} />
+          <input className={classes.fields} type="text" defaultValue={editUserValue.phone} onKeyUp={(e) => setEditUserValue({ ...editUserValue, phone: e.target.value })} />
+          <Button type="submit" variant="contained" color="primary" >SUBMIT</Button>
+        </form>
+      </Dialog>
     </div>
   );
 }
