d17082800d18a723eedd30eada1474253b9adfa7
Waleed Bin Alim <waleedbinalim@gmail.com>
2021-06-09 16:44:20 +0500

    removed not needed context

--- src/state/users/UserContext.js
+++ src/state/users/UserContext.js
@@ -1,21 +1,21 @@
-import React , {createContext , useState}from 'react';
+import React, { createContext, useState } from 'react';
 
 export const UserContext = createContext();
 
 const UserContextProvider = (props) => {
-    const [filteredUsers,setFilteredUsers] = useState([])
-    const [users,setUsers] = useState([])
-    const [userSearch , setUserSearch] = useState('')
-    const [openEditModal , setOpenEditModal] = useState(false)
-    const [editUserValue , setEditUserValue] = useState({name: 'blank', email: 'blank', city: 'blank', address: 'blank', phone: 'blank', })
+    // const [filteredUsers, setFilteredUsers] = useState([])
+    // const [users, setUsers] = useState([])
+    // const [userSearch, setUserSearch] = useState('')
+    const [openEditModal, setOpenEditModal] = useState(false)
+    const [editUserValue, setEditUserValue] = useState({ name: 'blank', email: 'blank', city: 'blank', address: 'blank', phone: 'blank', })
     return (
         <UserContext.Provider value={{
-                filteredUsers, setFilteredUsers,
-                users, setUsers,
-                userSearch, setUserSearch,
-                openEditModal, setOpenEditModal,
-                editUserValue, setEditUserValue
-            }}>
+            // filteredUsers, setFilteredUsers,
+            // users, setUsers,
+            // userSearch, setUserSearch,
+            openEditModal, setOpenEditModal,
+            editUserValue, setEditUserValue
+        }}>
             {props.children}
         </UserContext.Provider>
     )
