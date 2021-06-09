16bac79efe2f370dd59a04a843c3b93ea52afa57
Waleed Bin Alim <waleedbinalim@gmail.com>
2021-06-09 16:42:02 +0500

    Cleaned code format

--- src/components/orders/OrderForm.js
+++ src/components/orders/OrderForm.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { TextField, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core'
 import { useHistory } from "react-router-dom";
-import {headers ,api} from '../../api/Api'
+import { headers, api } from '../../utils/api/Api'
 
 let useStyles = makeStyles((theme) => {
-    return{
+    return {
         field: {
             marginTop: 20,
             marginBottom: 20,
@@ -21,16 +21,16 @@ let useStyles = makeStyles((theme) => {
 export default function OrderForm() {
     const history = useHistory()
     const classes = useStyles();
-    const [inputField , setInputField] = useState({
+    const [inputField, setInputField] = useState({
         deliveredTo: '',
         deliveredBy: ''
     })
 
     let handleSubmit = async (e) => {
         e.preventDefault();
-        if(inputField.deliveredTo && inputField.deliveredBy){
+        if (inputField.deliveredTo && inputField.deliveredBy) {
             let data = JSON.stringify(inputField)
-            await api.post('/orders/add', data , {headers: headers})
+            await api.post('/orders/add', data, { headers: headers })
             let addedUser = await api.get('orders/all')
             history.push('/orders')
         }
@@ -38,11 +38,11 @@ export default function OrderForm() {
 
     return (
         <div className="form-card">
-                <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-                    <TextField className={classes.field} label="Deliver To" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({...inputField , deliveredTo : e.target.value})} />
-                    <TextField className={classes.field} label="Deliver From" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({...inputField , deliveredBy : e.target.value})} />
-                    <Button type="submit" variant="contained" color="primary" className={classes.button}>SUBMIT</Button>
-                </form>
+            <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+                <TextField className={classes.field} label="Deliver To" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({ ...inputField, deliveredTo: e.target.value })} />
+                <TextField className={classes.field} label="Deliver From" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({ ...inputField, deliveredBy: e.target.value })} />
+                <Button type="submit" variant="contained" color="primary" className={classes.button}>SUBMIT</Button>
+            </form>
         </div>
     )
 }
--- src/components/users/UserForm.js
+++ src/components/users/UserForm.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { TextField, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core'
 import { useHistory } from "react-router-dom";
-import {headers ,api} from '../../api/Api'
+import { headers, api } from '../../utils/api/Api'
 
 let useStyles = makeStyles((theme) => {
-    return{
+    return {
         field: {
             marginTop: 20,
             marginBottom: 20,
@@ -23,7 +23,7 @@ let useStyles = makeStyles((theme) => {
 export default function UserForm() {
     const history = useHistory()
     const classes = useStyles();
-    const [inputField , setInputField] = useState({
+    const [inputField, setInputField] = useState({
         name: '',
         email: '',
         city: '',
@@ -34,9 +34,9 @@ export default function UserForm() {
 
     let handleSubmit = async (e) => {
         e.preventDefault();
-        if(inputField.name && inputField.email && inputField.city && inputField.address && inputField.phone){
+        if (inputField.name && inputField.email && inputField.city && inputField.address && inputField.phone) {
             let data = JSON.stringify(inputField)
-            await api.post('/users/add', data , {headers: headers})
+            await api.post('/users/add', data, { headers: headers })
             let addedUser = await api.get('users/all')
             console.log(addedUser.data)
             history.push('/users')
@@ -45,14 +45,14 @@ export default function UserForm() {
 
     return (
         <div className="form-card">
-                <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-                    <TextField className={classes.field} label="Name" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({...inputField , name : e.target.value})} />
-                    <TextField className={classes.field} label="Email" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({...inputField , email : e.target.value})} />
-                    <TextField className={classes.field} label="City" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({...inputField , city : e.target.value})} />
-                    <TextField className={classes.field} label="Address" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({...inputField , address : e.target.value})} />
-                    <TextField className={classes.field} label="Phone" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({...inputField , phone : e.target.value})} />
-                    <Button type="submit" className={classes.button} variant="contained" color="primary" >SUBMIT</Button>
-                </form>
+            <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+                <TextField className={classes.field} label="Name" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({ ...inputField, name: e.target.value })} />
+                <TextField className={classes.field} label="Email" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({ ...inputField, email: e.target.value })} />
+                <TextField className={classes.field} label="City" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({ ...inputField, city: e.target.value })} />
+                <TextField className={classes.field} label="Address" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({ ...inputField, address: e.target.value })} />
+                <TextField className={classes.field} label="Phone" variant="outlined" fullWidth required onKeyUp={(e) => setInputField({ ...inputField, phone: e.target.value })} />
+                <Button type="submit" className={classes.button} variant="contained" color="primary" >SUBMIT</Button>
+            </form>
         </div>
     )
 }
